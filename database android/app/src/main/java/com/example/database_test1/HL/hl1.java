0d0d6587ba1e57f718d96a74a7d6adf65e7cedee
jayger1132 <66061564+jayger1132@users.noreply.github.com>
2020-12-27 16:27:28 +0800

    新增gridview

--- database android/app/src/main/java/com/example/database_test1/HL/hl1.java
+++ database android/app/src/main/java/com/example/database_test1/HL/hl1.java
@@ -1,20 +1,30 @@
 package com.example.database_test1.HL;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
 import android.widget.Button;
+import android.widget.GridView;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
+import com.example.database_test1.ExpandableHeightGridView;
 import com.example.database_test1.R;
 
 public class hl1 extends AppCompatActivity {
     //先宣告要使用的 item
     private Button button;
     private TextView txtShow,txtprice,txttime,txtidentity,txtparkingspace,txtsex,txtmanagefee;
-
+    private ImageView imgShow;
+    private ExpandableHeightGridView gridView;
+    private int[] imgId={R.drawable.hl101,R.drawable.hl102,R.drawable.hl103,R.drawable.hl104,R.drawable.hl105,R.drawable.hl106};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,9 +38,22 @@ public class hl1 extends AppCompatActivity {
         txtparkingspace=(TextView)findViewById(R.id.txtparkingspace);
         txtsex=(TextView)findViewById(R.id.txtsex);
         txtmanagefee=(TextView)findViewById(R.id.txtmanagefee);
+
+        imgShow=(ImageView) findViewById(R.id.imgShow);
+        gridView =(ExpandableHeightGridView) findViewById(R.id.gridregion);
+
         button=(Button)findViewById(R.id.button);
         // 設定 button 的 Listener
         button.setOnClickListener(buttonListener);
+
+        // 建立自訂的 Adapter
+        MyAdapter adapter=new MyAdapter(this);
+        // 設定 GridView 的資料來源
+        gridView.setAdapter(adapter);
+        gridView.setExpanded(true);
+
+        // 建立 GridView 的 ItemClick 事件
+        gridView.setOnItemClickListener(gridviewListener);
 
         // 取得 bundle
         Intent intent=this.getIntent();
@@ -39,7 +62,7 @@ public class hl1 extends AppCompatActivity {
         Integer id=bundle.getInt("id");
         String str = selectR +"  "+id;
         txtShow.setText(str);
-        txtprice.setText("7,500 元/月\n");
+        txtprice.setText("7,500 元/月");
         txttime.setText("最短一年");
         txtidentity.setText("學生");
         txtparkingspace.setText("平面式停車位，費用另計");
@@ -47,6 +70,45 @@ public class hl1 extends AppCompatActivity {
         txtmanagefee.setText("桌子,椅子,衣櫃,床,電視,冰箱,冷氣,洗衣機,網路,第四台,沙發");
 
     }
+
+    class MyAdapter extends BaseAdapter {
+        private Context mContext;
+        public MyAdapter(Context c){
+            mContext=c;
+        }
+        @Override
+        public int getCount(){
+            return imgId.length; // 圖片共有多少張
+        }
+        @Override
+        public Object getItem(int position){
+            return position;
+        }
+
+        @Override
+        public long getItemId(int position){
+            return position; // 目前圖片索引
+        }
+        // 定義 GridView 顯示的圖片
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent){
+            ImageView iv = new ImageView(mContext);
+            iv.setImageResource(imgId[position]);
+            iv.setScaleType(ImageView.ScaleType.CENTER);
+            iv.setLayoutParams(new GridView.LayoutParams(600,540));
+            return iv;
+        }
+    }
+    //Gridview 的Listener
+    private GridView.OnItemClickListener gridviewListener=
+            new GridView.OnItemClickListener(){
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view,
+                                        int position, long id) {
+                    imgShow.setImageResource(imgId[position]);
+
+                }
+            };
 
     private  Button.OnClickListener buttonListener=new Button.OnClickListener(){
         public void onClick(View v){
--- database android/app/src/main/java/com/example/database_test1/ML/ml1.java
+++ database android/app/src/main/java/com/example/database_test1/ML/ml1.java
@@ -1,20 +1,30 @@
 package com.example.database_test1.ML;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
 import android.widget.Button;
+import android.widget.GridView;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.database_test1.ExpandableHeightGridView;
+import com.example.database_test1.HL.hl1;
 import com.example.database_test1.R;
 
 public class ml1 extends AppCompatActivity {
     //先宣告要使用的 item
     private Button button;
     private TextView txtShow,txtprice,txttime,txtidentity,txtparkingspace,txtsex,txtmanagefee;
-
+    private ImageView imgShow;
+    private ExpandableHeightGridView gridView;
+    private int[] imgId={R.drawable.ml101,R.drawable.ml102,R.drawable.ml103,R.drawable.ml104,R.drawable.ml105,R.drawable.ml106};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,10 +38,19 @@ public class ml1 extends AppCompatActivity {
         txtparkingspace=(TextView)findViewById(R.id.txtparkingspace);
         txtsex=(TextView)findViewById(R.id.txtsex);
         txtmanagefee=(TextView)findViewById(R.id.txtmanagefee);
+        imgShow=(ImageView) findViewById(R.id.imgShow);
+        gridView =(ExpandableHeightGridView) findViewById(R.id.gridregion);
         button=(Button)findViewById(R.id.button);
         // 設定 button 的 Listener
         button.setOnClickListener(buttonListener);
+        // 建立自訂的 Adapter
+        MyAdapter adapter=new MyAdapter(this);
+        // 設定 GridView 的資料來源
+        gridView.setAdapter(adapter);
+        gridView.setExpanded(true);
 
+        // 建立 GridView 的 ItemClick 事件
+        gridView.setOnItemClickListener(gridviewListener);
         // 取得 bundle
         Intent intent=this.getIntent();
         Bundle bundle=intent.getExtras();
@@ -47,6 +66,44 @@ public class ml1 extends AppCompatActivity {
         txtmanagefee.setText("桌子,椅子,衣櫃,床,沙發,熱水器,天然瓦斯,電視,冰箱,冷氣,洗衣機");
 
     }
+    class MyAdapter extends BaseAdapter {
+        private Context mContext;
+        public MyAdapter(Context c){
+            mContext=c;
+        }
+        @Override
+        public int getCount(){
+            return imgId.length; // 圖片共有多少張
+        }
+        @Override
+        public Object getItem(int position){
+            return position;
+        }
+
+        @Override
+        public long getItemId(int position){
+            return position; // 目前圖片索引
+        }
+        // 定義 GridView 顯示的圖片
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent){
+            ImageView iv = new ImageView(mContext);
+            iv.setImageResource(imgId[position]);
+            iv.setScaleType(ImageView.ScaleType.CENTER);
+            iv.setLayoutParams(new GridView.LayoutParams(600,540));
+            return iv;
+        }
+    }
+    //Gridview 的Listener
+    private GridView.OnItemClickListener gridviewListener=
+            new GridView.OnItemClickListener(){
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view,
+                                        int position, long id) {
+                    imgShow.setImageResource(imgId[position]);
+
+                }
+            };
 
     private  Button.OnClickListener buttonListener=new Button.OnClickListener(){
         public void onClick(View v){
